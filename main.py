fb64c22d0f391525f347b4fb8d8f539e9b16be88
Diamondwolf-1975 <esserjohn@ymail.com>
2025-04-03 12:15:34 -0400

    MadLibs solution
    Next step in Python education video.

--- main.py
+++ main.py
@@ -3,16 +3,4 @@
 #2345678901234567890123456789012345678901234567890123456789012345678901234567890
 # URL https://www.youtube.com/watch?v=ix9cRaBkVe0
 
-# Madlibs game - filling phrases with random words
-# Build the input statements needed
-adjective1 = input("Enter an adjective (description): ")
-noun1 = input("Enter a noun (person, place, or thing): ")
-adjective2 = input("Enter another adjective (description): ")
-verb1 = input("Enter a verb ending in 'ing': ")
-adjective3 = input("Enter a 3rd adjective (description): ")
-# Create a story template
-print(f"Today I went to a {adjective1} zoo.")
-print(f"In an exhibit, I saw a {noun1}")
-print(f"{noun1} was {adjective2} and {verb1}")
-print(f"I was {adjective3}!")
 # Restart at time index 37:55
